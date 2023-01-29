c73f90e3832edd7553bdb660bb8d8e9dc0e38fb8
GhaliMahdi <ghali.mahdi99@gmail.com>
2023-01-30 00:35:53 +0100

    Added the playRound function with the logic of playing Rock Paper Scissors, and it returns Draw, you win or you lose.

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 function getComputerChoice() {
     const options = ['Rock', 'Paper', 'Scissors'];
     let theChoice = options[Math.floor(Math.random() * 3)];
-    console.log(theChoice)
+    return theChoice
 }
 
-getComputerChoice()
+const playerSelection = 'RoCk';
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock') {
+        return ("Draw");
+    }
+    else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+        return ("You Win")
+    }
+    else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+        return ("You Lose")
+    }
+    else if(playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'paper') {
+        return ("Draw");
+    }
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+        return ("You Win")
+    }
+    else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
+        return ("You Lose")
+    }
+    else if(playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'scissors') {
+        return ("Draw");
+    }
+    else if (playerSelection.toLowerCase() === 'scissors' && (computerSelection.toLowerCase() === 'paper')) {
+        return ("You Win")
+    }
+    else if (playerSelection.toLowerCase() === 'scissors' && (computerSelection.toLowerCase() === 'rock')) {
+        return ("You Lose")
+    }
+}
+
+
+console.log(playRound(playerSelection, computerSelection));
