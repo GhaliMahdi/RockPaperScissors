15044c668b034655343eb0d6b86822df05573fbf
GhaliMahdi <ghali.mahdi99@gmail.com>
2023-01-30 00:54:55 +0100

    Added the game function which plays the game five times and counts who wins between human and pc.

--- index.js
+++ index.js
@@ -4,38 +4,89 @@ function getComputerChoice() {
     return theChoice
 }
 
-const playerSelection = 'RoCk';
-const computerSelection = getComputerChoice();
-
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, countHuman, countPc) {
     if(playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock') {
         return ("Draw");
     }
     else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+
         return ("You Win")
     }
     else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+
         return ("You Lose")
     }
     else if(playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'paper') {
         return ("Draw");
     }
     else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+
         return ("You Win")
     }
     else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
+ 
         return ("You Lose")
     }
     else if(playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'scissors') {
         return ("Draw");
     }
     else if (playerSelection.toLowerCase() === 'scissors' && (computerSelection.toLowerCase() === 'paper')) {
+
         return ("You Win")
     }
     else if (playerSelection.toLowerCase() === 'scissors' && (computerSelection.toLowerCase() === 'rock')) {
+
         return ("You Lose")
     }
 }
 
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let countPc = 0;
+    let countHuman = 0;
+
+    for (let i = 0; i < 5; i++) {
+        
+        const playerSelection = prompt("What's your choice for this round ?")
+        const computerSelection = getComputerChoice();
+
+        if(playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock') {
+             
+        }
+        else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+            countHuman++;
+        }
+        else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+            countPc++;
+        }
+        else if(playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'paper') {
+            
+        }
+        else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+            countHuman++;
+        }
+        else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
+            countPc++;
+        }
+        else if(playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'scissors') {
+            
+        }
+        else if (playerSelection.toLowerCase() === 'scissors' && (computerSelection.toLowerCase() === 'paper')) {
+            countHuman++;
+        }
+        else if (playerSelection.toLowerCase() === 'scissors' && (computerSelection.toLowerCase() === 'rock')) {
+             countPc++;
+        }
+
+        
+        console.log(playRound(playerSelection, computerSelection), `Round ${i+1}`);
+     }
+     
+     if (countPc > countHuman) {
+        console.log("Computer Wins.");
+     } else if (countHuman > countPc) {
+        console.log("Human Wins.")
+     } else console.log("Draw.");
+}
+
+game();
